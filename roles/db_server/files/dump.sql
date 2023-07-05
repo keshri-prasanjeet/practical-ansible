e6042ce10738b1a96ba90f56f86d154760d0a264
root <root@Prasanjeet-pc.lan.cleo.com>
2023-07-06 01:37:59 +0530

    This is a placeholder commit message

---
+++ roles/db_server/files/dump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS test (
+  message varchar(255) NOT NULL,
+  PRIMARY KEY (message)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO test(message) VALUES('Ansible To Do List New');
+INSERT INTO test(message) VALUES('Get ready New');
+INSERT INTO test(message) VALUES('Ansible is fun New');
+
---
+++ roles/web_server/files/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import pymysql
+
+# Print neccessary headers
+
+print("Content-Type: text/html")
+print()
+
+
+# Connect to the database.
+conn = pymysql.connect(
+        db='testdb',
+        user='root',
+        passwd='password',
+        host='localhost')
+c = conn.cursor()
+
+# Print the contents of the table.
+c.execute("SELECT * FROM test;")
+for i in c:
+    print(i)
